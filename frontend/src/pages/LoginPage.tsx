4587d32cfaca5fecb9ffb83997fb054ca92d6ce2
madev7 <madazv7@gmail.com>
2024-12-09 16:51:46 -0500

    [add] adición de pagina para subida de libros, conección de registro e inicio de sesión con el backend, implementación de rutas protegidas y página con info acerca de al aplicación.

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,13 @@
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
+import { useNavigate } from 'react-router-dom';
 
+// Interfaz con los datos del formulario de inicio de sesión.
 interface LoginFormInputs {
     email: string;
     password: string;
 }
 
+// Interfaz con los datos del formulario de registro.
 interface RegisterFormInputs {
     name: string;
     email: string;
@@ -12,30 +15,73 @@ interface RegisterFormInputs {
     confirmPassword: string;
 }
 
+// Función genérica para manejar login y registro
+const handleAuth = async (url: string, data: any, navigate: any) => {
+    try {
+        // Realiza una solicitud HTTP POST al servidor para login o registro
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        // Obtiene la respuesta del servidor en formato JSON
+        const result = await response.json();
+
+        // Verifica si la respuesta fue exitosa
+        if (response.ok) {
+            // Almacena el token de autenticación en el almacenamiento local
+            localStorage.setItem('authToken', result.token);
+
+            // Redirige al usuario a la página principal después de un inicio de sesión o registro exitoso
+            navigate('/');
+        } else {
+            // Maneja el caso en el que la respuesta no es exitosa
+            alert(result.message);
+        }
+    } catch (error) {
+        // Captura y maneja cualquier error que ocurra durante la solicitud HTTP
+        console.error("Error al hacer la solicitud:", error);
+    }
+};
+
+// Componente principal de la página de inicio de sesión
 export default function LoginPage() {
 
-    // React Hook Form para inicio de sesión
+    // Instancia del hook useNavigate para manejar la navegación entre rutas.
+    const navigate = useNavigate();
+
+    // Define la URL del backend desde las variables de entorno
+    const backUrl = import.meta.env.VITE_API_URL;
+
+    // Configuración de React Hook Form para el formulario de inicio de sesión
     const {
         register: registerLogin,
         handleSubmit: handleLoginSubmit,
         formState: { errors: loginErrors },
     } = useForm<LoginFormInputs>();
 
-    // React Hook Form para registro
+    // Configuración de React Hook Form para el formulario de registro
     const {
         register: registerRegister,
         handleSubmit: handleRegisterSubmit,
         formState: { errors: registerErrors },
     } = useForm<RegisterFormInputs>();
 
-    // Función para manejar inicio de sesión
+    // Función que maneja el inicio de sesión cuando se envía el formulario
     const handleLogin = (data: LoginFormInputs) => {
-        console.log("Datos de inicio de sesión:", data);
+        handleAuth(`${backUrl}/auth/login`, data, navigate);
     };
 
-    // Función para manejar registro
+    // Función que maneja el registro cuando se envía el formulario
     const handleRegister = (data: RegisterFormInputs) => {
-        console.log("Datos de registro:", data);
+        if (data.password !== data.confirmPassword) {
+            console.log('Las contraseñas no coinciden');
+            return; // Detener la ejecución si las contraseñas no coinciden
+        }
+        handleAuth(`${backUrl}/auth/register`, data, navigate);
     };
 
     return (
